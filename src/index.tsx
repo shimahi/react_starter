def3d5c66231819bc891aed10411b8dbcb5aa078
shiimahi <shimahi@rear.jp>
2020-05-17 02:57:30 +0900

    タイトルとか修正

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import 'assets/css/foundation/global.pcss'
 import React from 'react'
 import { render } from 'react-dom'
 
-import Hero from './components/Hero'
 
-const App = () => <Hero />
+const App = () => <h1>Hello React</h1>
 
 render(<App />, document.getElementById('root'))
