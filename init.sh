065453f78601e35d3b55d5c4112fdb81e2a834c9
shiimahi <shimahi@rear.jp>
2020-05-23 01:11:16 +0900

    @types/react-dom追加

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yarn add react react-dom
-yarn add -D @types/core-js @types/node @types/react autoprefixer clean-webpack-plugin core-js css-loader css-mqpacker cssnano doiuse file-loader glob html-webpack-plugin postcss-comment postcss-custom-media postcss-easy-import postcss-loader postcss-nesting postcss-pxtorem postcss-simple-vars ress style-loader ts-loader typescript url-loader webpack webpack-cli webpack-dev-server webpack-merge worker-plugin
+yarn add -D @types/core-js @types/node @types/react @types/react-dom autoprefixer clean-webpack-plugin core-js css-loader css-mqpacker cssnano doiuse file-loader glob html-webpack-plugin postcss-comment postcss-custom-media postcss-easy-import postcss-loader postcss-nesting postcss-pxtorem postcss-simple-vars ress style-loader ts-loader typescript url-loader webpack webpack-cli webpack-dev-server webpack-merge worker-plugin
 
 rm -rf .git
 rm init.sh
