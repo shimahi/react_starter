cba3a7e359f452c27d02598fc4d32469aedc0f84
shiimahi <shimahi@rear.jp>
2020-05-27 17:45:39 +0900

    README更新

---
+++ README.md
@@ -0,0 +1,15 @@
+# REACT STARTER
+
+Webpackを用いた TypeScript + React + Apollo + PostCSS アプリの環境構築を `create-react-app` を使わずに行えるシェルスクリプトです。このリポジトリをクローンして `init.sh` を実行してください。
+
+```
+$ git@github.com:shimahi/react_starter.git
+```
+
+```
+$ mv react_sterter your_app_name && cd your_app_name
+```
+
+```
+$ sh init.sh
+```
--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 ### remove original git information
 rm -rf .git
 rm .gitignore
-
+rm README.md
 
 ### initialize package.json
 yarn init -y
@@ -240,8 +240,11 @@ echo '@custom-media --xxs (max-width: 413.8px);
 
 yarn add react react-dom
 
-yarn add -D typescript webpack webpack-cli ts-loader webpack-dev-server html-webpack-plugin style-loader css-loader @types/node url-loader file-loader postcss-loader worker-plugin webpack-merge dotenv-webpack @types/react @types/react-dom autoprefixer css-mqpacker cssnano glob postcss-comment postcss-custom-media postcss-easy-import postcss-nesting postcss-pxtorem postcss-custom-properties ress strip-ansi doiuse
+yarn add -D typescript webpack webpack-cli ts-loader webpack-dev-server html-webpack-plugin style-loader css-loader @types/node url-loader file-loader postcss-loader worker-plugin webpack-merge dotenv-webpack @types/react @types/react-dom autoprefixer css-mqpacker cssnano glob postcss-comment postcss-custom-media postcss-easy-import postcss-nesting postcss-pxtorem postcss-custom-properties ress strip-ansi doiuse apollo-client react-apollo graphql graphql-tag @apollo/react-hooks apollo-cache-inmemory apollo-link-http
 
+
+touch README.md
+echo '# My New React App' >> README.md
 
 ### remove this script
 rm init.sh
