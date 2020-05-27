37128184b39092e395dd377cc245e8cf07952ae2
shiimahi <shimahi@rear.jp>
2020-05-27 17:21:15 +0900

    1から環境構築を行うシェルスクリプトを作成

--- old/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import 'ress'
-import 'assets/css/foundation/global.pcss'
-import React from 'react'
-import { render } from 'react-dom'
-
-
-const App = () => <h1>Hello React</h1>
-
-render(<App />, document.getElementById('root'))
