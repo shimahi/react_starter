fd784e5c9bf821314becd692ed3189107beece65
shiimahi <shimahi@rear.jp>
2020-05-27 18:30:07 +0900

    READMEのタイトルが改行しないように修正2

--- init.sh
+++ init.sh
@@ -243,8 +243,9 @@ yarn add react react-dom
 yarn add -D typescript webpack webpack-cli ts-loader webpack-dev-server html-webpack-plugin style-loader css-loader @types/node url-loader file-loader postcss-loader worker-plugin webpack-merge dotenv-webpack @types/react @types/react-dom autoprefixer css-mqpacker cssnano glob postcss-comment postcss-custom-media postcss-easy-import postcss-nesting postcss-pxtorem postcss-custom-properties ress strip-ansi doiuse apollo-client react-apollo graphql graphql-tag @apollo/react-hooks apollo-cache-inmemory apollo-link-http
 
 ### write README
+var=basename`pwd`
+str=`basename ${var}`
+echo "#" ${str} >> README.md
 
-echo -n '# ' >> README.md
-basename `pwd` >> README.md
 ### remove this script
 rm init.sh
