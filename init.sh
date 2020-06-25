01736df395040444e3e8306b4d5a1366811e4848
shiimahi <shimahi@rear.jp>
2020-06-26 00:09:12 +0900

    echo周りのインデントを調整

--- init.sh
+++ init.sh
@@ -23,163 +23,163 @@ else
   touch .blowserslistrc .gitignore .postcssrc tsconfig.json webpack.common.js webpack.dev.js webpack.prod.js
 
   echo 'last 2 version
-  > 1%
-  ' >>.blowserslistrc
+> 1%
+' >>.blowserslistrc
 
   echo 'node_modules
-  dist/*
-  yarn-error.log
-  .DS_Store
-  .vscode
-  ' >>.gitignore
+dist/*
+yarn-error.log
+.DS_Store
+.vscode
+' >>.gitignore
 
   echo '{
-    "modules": true,
-    "parser": "postcss-comment",
-    "plugins": {
-      "postcss-pxtorem": {},
-      "postcss-nested": {},
-      "postcss-custom-media": {
-        "importFrom": "./src/assets/css/foundation/media.css"
-      },
-      "postcss-custom-properties": {},
-      "css-mqpacker": {},
-      "cssnano": {
-        "preset": [
-          "default", {"discardComments": {"removeAll": true}}
-        ]
-      },
-      "autoprefixer": {
-        "grid": "autoplace"
-      },
-      "doiuse": {}
-    }
+  "modules": true,
+  "parser": "postcss-comment",
+  "plugins": {
+    "postcss-pxtorem": {},
+    "postcss-nested": {},
+    "postcss-custom-media": {
+      "importFrom": "./src/assets/css/foundation/media.css"
+    },
+    "postcss-custom-properties": {},
+    "css-mqpacker": {},
+    "cssnano": {
+      "preset": [
+        "default", {"discardComments": {"removeAll": true}}
+      ]
+    },
+    "autoprefixer": {
+      "grid": "autoplace"
+    },
+    "doiuse": {}
   }
-  ' >>.postcssrc
+}
+' >>.postcssrc
 
   echo '{
-    "compilerOptions": {
-      "target": "es2019",
-      "module": "esnext",
-      "moduleResolution": "node",
-      "jsx": "react",
-      "strict": true,
-      "esModuleInterop": true,
-      "forceConsistentCasingInFileNames": true,
-      "baseUrl": "src",
-      "paths": {
-        "@/*": ["src/*"]
-      },
-      "typeRoots": ["types", "mode_modules/@types"],
-      "resolveJsonModule": true
-    }
+  "compilerOptions": {
+    "target": "es2019",
+    "module": "esnext",
+    "moduleResolution": "node",
+    "jsx": "react",
+    "strict": true,
+    "esModuleInterop": true,
+    "forceConsistentCasingInFileNames": true,
+    "baseUrl": "src",
+    "paths": {
+      "@/*": ["src/*"]
+    },
+    "typeRoots": ["types", "mode_modules/@types"],
+    "resolveJsonModule": true
   }
-  ' >>tsconfig.json
+}
+' >>tsconfig.json
 
   echo "const path = require('path')
-  const WorkerPlugin = require('worker-plugin')
+const WorkerPlugin = require('worker-plugin')
 
-  module.exports = {
-    entry: './src/index.tsx',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
           },
         },
-        {
-          test: /\.(css|pcss)/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                url: true,
-                importLoaders: 1,
-              },
-            },
-            'postcss-loader',
-          ],
-        },
-        {
-          test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 100 * 1024,
-                name: './img/[name].[ext]',
-              },
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.ts', '.tsx', '.json', '.mjs', '.wasm'],
-      modules: [
-        path.resolve(__dirname, 'src'),
-        path.resolve(__dirname, 'node_modules'),
-      ],
-      alias: {
-        src: path.resolve(__dirname, 'src'),
       },
+      {
+        test: /\.(css|pcss)/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              url: true,
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100 * 1024,
+              name: './img/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json', '.mjs', '.wasm'],
+    modules: [
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
     },
-    plugins: [new WorkerPlugin()],
-  }
-  " >>webpack.common.js
+  },
+  plugins: [new WorkerPlugin()],
+}
+" >>webpack.common.js
 
   echo "const path = require('path')
-  const merge = require('webpack-merge')
-  const common = require('./webpack.common.js')
-  const HTMLPlugin = require('html-webpack-plugin')
-  const Dotenv = require('dotenv-webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const HTMLPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
-  module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      watchContentBase: true,
-    },
-    plugins: [
-      new HTMLPlugin({
-        template: path.join(__dirname, 'src/index.html'),
-      }),
-      new Dotenv({
-        path: path.join(__dirname, '.env.development'),
-      }),
-    ],
-  })
-  " >>webpack.dev.js
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    watchContentBase: true,
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+    }),
+    new Dotenv({
+      path: path.join(__dirname, '.env.development'),
+    }),
+  ],
+})
+" >>webpack.dev.js
 
   echo "const path = require('path')
-  const merge = require('webpack-merge')
-  const common = require('./webpack.common.js')
-  const Dotenv = require('dotenv-webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const Dotenv = require('dotenv-webpack')
 
-  module.exports = merge(common, {
-    mode: 'production',
-    output: {
-      publicPath: '/dist/',
-    },
-    devtool: 'none',
-    plugins: [
-      new Dotenv({
-        path: path.join(__dirname, '.env.production'),
-      }),
-    ],
-  })
-  " >>webpack.prod.js
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    publicPath: '/dist/',
+  },
+  devtool: 'none',
+  plugins: [
+    new Dotenv({
+      path: path.join(__dirname, '.env.production'),
+    }),
+  ],
+})
+" >>webpack.prod.js
 
   ### make source files
   mkdir -p src/assets/css/foundation types
@@ -188,17 +188,17 @@ else
     types/style.css.d.ts types/images.d.ts
 
   echo '<!DOCTYPE html>
-  <html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>いぬ</title>
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-  </html>
-  ' >>src/index.html
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>いぬ</title>
+</head>
+<body>
+  <div id="root"></div>
+</body>
+</html>
+' >>src/index.html
 
   echo "import 'ress'
   import 'assets/css/foundation/global.css'
@@ -214,41 +214,41 @@ else
   " >>src/index.tsx
 
   echo 'a {
-    text-decoration: none;
-  }
-  ' >>src/assets/css/foundation/global.css
+  text-decoration: none;
+}
+' >>src/assets/css/foundation/global.css
 
   echo '@custom-media --xxs (max-width: 413.8px);
-  @custom-media --xs (max-width: 575.98px);
+@custom-media --xs (max-width: 575.98px);
 
-  @custom-media --sm (min-width: 575.98px);
-  @custom-media --md (min-width: 767.98px);
-  @custom-media --lg (min-width: 991.98px);
-  @custom-media --xl (min-width: 1199.98px);
-  ' >>src/assets/css/foundation/media.css
+@custom-media --sm (min-width: 575.98px);
+@custom-media --md (min-width: 767.98px);
+@custom-media --lg (min-width: 991.98px);
+@custom-media --xl (min-width: 1199.98px);
+' >>src/assets/css/foundation/media.css
 
   echo "declare module '*.png'
-  declare module '*.jpg'
-  declare module '*.gif'
-  declare module '*.svg'
-  " >>types/images.d.ts
+declare module '*.jpg'
+declare module '*.gif'
+declare module '*.svg'
+" >>types/images.d.ts
 
   echo "declare module '*.css' {
-    interface IClassNames {
-      [className: string]: string
-    }
-    const classNames: IClassNames
-    export = classNames
-  }
+interface IClassNames {
+  [className: string]: string
+}
+const classNames: IClassNames
+export = classNames
+}
 
-  declare module '*.pcss' {
-    interface IClassNames {
-      [className: string]: string
-    }
-    const classNames: IClassNames
-    export = classNames
-  }
-  " >>types/style.css.d.ts
+declare module '*.pcss' {
+interface IClassNames {
+  [className: string]: string
+}
+const classNames: IClassNames
+export = classNames
+}
+" >>types/style.css.d.ts
 
   ### import npm packages
 
